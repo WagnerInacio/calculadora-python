8fc92eb2706f541ae4c9b6b18904467be1ecf643
Wagner Inacio <wagner.inacio.silva@outlook.com>
2023-03-05 10:09:34 -0300

    commit inicial

---
+++ calculadora.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+
+# Cria a janela principal
+window = tk.Tk()
+window.title("Calculadora")
+
+# Cria o frame para o display
+display_frame = tk.Frame(window)
+display_frame.grid(row=0, column=0, columnspan=4, padx=5, pady=5)
+
+# Cria o display
+display = tk.Entry(display_frame, width=15, font=("Arial", 16))
+display.pack(fill=tk.X, padx=5, pady=5)
+
+# Função para atualizar o display com um novo valor
+def update_display(value):
+    current = display.get()
+    display.delete(0, tk.END)
+    display.insert(0, str(current) + str(value))
+
+# Função para calcular o resultado da expressão no display
+def calculate():
+    result = eval(display.get())
+    display.delete(0, tk.END)
+    display.insert(0, result)
+
+# Cria os botões para os números de 1 a 9
+number_buttons = []
+for i in range(1, 10):
+    button = tk.Button(window, text=str(i), width=5, height=2, command=lambda i=i: update_display(i))
+    button.grid(row=(3 - (i-1)//3) + 1, column=(i-1)%3, padx=1, pady=1)
+    number_buttons.append(button)
+
+# Cria o botão para o número zero
+tk.Button(window, text="0", width=5, height=2, command=lambda: update_display("0")).grid(row=5, column=0, padx=1, pady=1)
+
+# Cria os botões para as operações de adição, subtração e multiplicação
+operator_buttons = []
+operators = ["+", "-", "*"]
+for i in range(len(operators)):
+    button = tk.Button(window, text=operators[i], width=5, height=2,
+                       command=lambda i=i: update_display(operators[i]))
+    button.grid(row=i+2, column=3,padx=1, pady=1)
+    operator_buttons.append(button)
+
+# Cria o botão para a operação de divisão
+tk.Button(window, text="/", width=5, height=2, command=lambda: update_display("/")).grid(row=5, column=1, padx=1, pady=1)
+
+# Cria o botão para o ponto decimal
+tk.Button(window, text=".", width=5, height=2, command=lambda: update_display(".")).grid(row=5,column=2, padx=1, pady=1)
+
+# Cria o botão para o resultado da expressão
+tk.Button(window, text="=", width=5, height=2, command=calculate).grid(row=5, column=3, padx=1,pady=1)
+
+# Cria o botão para limpar o display
+tk.Button(window, text="AC", width=5, height=2, command=lambda: display.delete(0, tk.END)).grid(row=1, column=3, padx=1, pady=1)
+
+# Inicia o loop principal da janela
+window.mainloop()
